6a51f349fb1ea7fd9c9df18f3f001032f1f527bc
Prince-NP001 <122963559+Prince-NP001@users.noreply.github.com>
2023-01-24 10:16:39 +0530

    new changes

---
+++ src/Components/Home/Bannner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Bannner(props) {
+  return <div className="banner-page">{props.name}</div>;
+}
+
+function BannnerType2() {
+  return <div>Bannner</div>;
+}
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import Banner from "./Bannner.jsx";
+import "../../styles/Home.css";
 
 export default function Home() {
+  const [Slider_position, setSlider_position] = useState(0);
+
+  const left_slider_push_button = () => {
+    document.querySelector(".home-banner-slider").style.transform =
+      "translateX(calc(" + Slider_position + "vw - 100vw))";
+    setSlider_position(Slider_position + 100);
+  };
+  const right_slider_push_button = () => {
+    document.querySelector(".home-banner-slider").style.transform =
+      "translateX(calc(" + Slider_position + "vw + 100vw))";
+    setSlider_position(Slider_position + 100);
+  };
+
   return (
     <>
-      <div className="banner">{["first", ""]}</div>
+      <section className="banner-slider">
+        <button
+          onClick={left_slider_push_button}
+          className="slider-push-button"
+        >
+          &larr;
+        </button>
+        <div className="home-banner-slider">
+          {[
+            { id: 1, name: "first" },
+            { id: 2, name: "outcomes" },
+            { id: 3, name: "ect" },
+            { id: 4, name: "if&se" },
+            { id: 5, name: "ss&cp" },
+            { id: 6, name: "dl" },
+            { id: 7, name: "gp" },
+            { id: 8, name: "last" },
+          ].map((e) => {
+            return <Banner key={e.id} index={e.id} name={e.name} />;
+          })}
+        </div>
+        <button
+          onClick={right_slider_push_button}
+          className="slider-push-button"
+        >
+          &rarr;
+        </button>
+      </section>
     </>
   );
 }
