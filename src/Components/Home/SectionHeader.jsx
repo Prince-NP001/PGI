d39d933de1fdd6ed868ed875abeeab4940b30513
Prince-NP001 <122963559+Prince-NP001@users.noreply.github.com>
2023-01-30 18:00:04 +0530

    changing to mui

--- src/Components/Home/SectionHeader.jsx
+++ src/Components/Home/SectionHeader.jsx
@@ -15,16 +15,46 @@ import {
 export default function SectionHeader(props) {
   const [selectValue, setSelectValue] = useState("2021-22");
 
+  const buttonsColorSetting = () => {
+    let headerIcon = document.querySelector(".chart-graph-btn-container");
+    if (!props.chartOrListTrigger) {
+      headerIcon = headerIcon.firstElementChild;
+      headerIcon.style.backgroundColor = "#7e84a3";
+      headerIcon.style.color = "white";
+      headerIcon.nextSibling.style.backgroundColor = "white";
+      headerIcon.nextSibling.style.color = "#7e84a3";
+    } else {
+      headerIcon = headerIcon.lastElementChild;
+      headerIcon.style.backgroundColor = "#7e84a3";
+      headerIcon.style.color = "white";
+      headerIcon.previousSibling.style.backgroundColor = "white";
+      headerIcon.previousSibling.style.color = "#7e84a3";
+    }
+  };
+
+  const handleClickMap = () => {
+    if (!props.chartOrListTrigger) {
+      props.ClickTrigger();
+      buttonsColorSetting();
+    }
+  };
+  const handleClickBar = () => {
+    if (props.chartOrListTrigger) {
+      props.ClickTrigger();
+      buttonsColorSetting();
+    }
+  };
+
   return (
     <Box className="chart-graph-header">
       <Typography component={"h2"}>{props.title}</Typography>
       <Typography component={"h2"}>{props.mid_title}</Typography>
       <Box className="chart-graph-buttons-and-select">
         <Box className="chart-graph-btn-container">
-          <Button onClick={props.ClickTrigger}>
+          <Button onClick={handleClickMap}>
             <BarChartIcon />
           </Button>
-          <Button onClick={props.ClickTrigger}>
+          <Button onClick={handleClickBar}>
             <FormatListBulletedIcon />
           </Button>
         </Box>
