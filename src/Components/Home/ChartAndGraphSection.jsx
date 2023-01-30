3802b81f0cf77698210ba651f84bf570cba13d7f
Prince-NP001 <122963559+Prince-NP001@users.noreply.github.com>
2023-01-30 14:25:30 +0530

    changing to mui

--- src/Components/Home/ChartAndGraphSection.jsx
+++ src/Components/Home/ChartAndGraphSection.jsx
@@ -2,70 +2,14 @@ import React, { useState } from "react";
 import "../../styles/Home.scss";
 import IndiaMaps from "./IndiaMaps.jsx";
 import IndiaBarChart from "./IndiaBarChart.jsx";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 import NationalListView from "./NationalListView.jsx";
-import {
-  Box,
-  Button,
-  FormControl,
-  MenuItem,
-  Select,
-  Typography,
-} from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import SectionHeader from "./SectionHeader";
 
 export default function ChartAndGraphSection() {
-  const [selectValue, setSelectValue] = useState("2021-22");
-  const chart_and_graph_view = (e) => {
-    e.currentTarget.style.backgroundColor = "#7e84a3";
-    e.currentTarget.style.color = "white";
-    e.currentTarget.nextSibling.style.backgroundColor = "white";
-    e.currentTarget.nextSibling.style.color = "#7e84a3";
-
-    document.querySelector(
-      "#national-charts-and-graph-list-view"
-    ).style.display = "none";
-    document.querySelector("#national-charts-and-graph").style.display = "flex";
-  };
-  const list_view = (e) => {
-    e.currentTarget.style.backgroundColor = "#7e84a3";
-    e.currentTarget.style.color = "white";
-    e.currentTarget.previousSibling.style.backgroundColor = "white";
-    e.currentTarget.previousSibling.style.color = "#7e84a3";
-
-    document.querySelector(
-      "#national-charts-and-graph-list-view"
-    ).style.display = "flex";
-    document.querySelector("#national-charts-and-graph").style.display = "none";
-  };
-
   return (
     <Box className="chart-graph-content">
-      <Box className="chart-graph-header">
-        <Typography component={"h2"}>PGI-District Grading</Typography>
-        <Typography component={"h2"}>National</Typography>
-        <Box className="chart-graph-buttons-and-select">
-          <Box className="chart-graph-btn-container">
-            <Button onClick={chart_and_graph_view}>
-              <BarChartIcon />
-            </Button>
-            <Button onClick={list_view}>
-              <FormatListBulletedIcon />
-            </Button>
-          </Box>
-          <FormControl fullWidth>
-            <Select
-              name="show-month-wise"
-              value={selectValue}
-              onChange={(e) => setSelectValue(e.target.value)}
-            >
-              <MenuItem value={"2021-22"}>2021-22</MenuItem>
-              <MenuItem value={"2020-21"}>2020-21</MenuItem>
-              <MenuItem value={"2019-20"}>2019-20</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-      </Box>
+      <SectionHeader title="PGI-District Grading" mid_title="National" />
       <Box>
         <Box className="charts-and-graph" id="national-charts-and-graph">
           <Box className="map-and-chart-bundle">
